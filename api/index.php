bca24e3d087460563f067be7892b472b1cb602f9
dirkarnez <smalldirkalex@gmail.com>
2020-08-07 13:33:37 +0800

    - add sample code

--- api/index.php
+++ api/index.php
@@ -1,3 +1,22 @@
 <?php
-    phpinfo();
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\App;
+
+$app->get('/names/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+
+    $names = array("Peter", "Paul", "Mary");
+    array_push($names, $name);
+
+    sort($names);
+
+    return $response->withJson($names);
+});
+
+$app->run();
+
 ?>
